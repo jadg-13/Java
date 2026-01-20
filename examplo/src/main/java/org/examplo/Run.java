fb8d7d3fbef572fe798a1085c413871fa81e1ad4
jadg13 <jadg13@hotmail.com>
2026-01-19 20:33:48 -0600

    sesion 1

---
+++ examplo/src/main/java/org/examplo/Run.java
@@ -0,0 +1,16 @@
+package org.examplo;
+
+public class Run {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo...!");
+        int number1 = 10;
+        System.out.println("Numero 1: " + number1);
+        float number2 = 20.5f;
+        System.out.println("Numero 2: " + number2);
+        double number3 = 10.5;
+        System.out.println("Numero 3: " + number3);
+        String name = "jadg";
+        System.out.println("Nombre: " + name);
+    }
+}
